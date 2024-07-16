a8824eb747039fd012cdd848d47dab555d6db234
sila7 <sila7@SILA>
2024-07-16 23:04:35 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+using System;
+
+namespace condprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 10)
+            {
+                Console.WriteLine("Girilen sayı 10'dan büyüktür. ");
+                if (sayi % 2 == 0)
+                {
+                    Console.WriteLine("Girilen sayı çifttir.");
+
+                }
+                else { Console.WriteLine("Girilen sayı tektir."); }
+
+
+            }
+            else if (sayi == 10)
+            {
+                Console.WriteLine("Girilen sayı 10'a eşittir. ");
+                Console.WriteLine("Girilen sayı çifttir.");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Girilen sayı 10'dan küçüktür. ");
+                if (sayi % 2 == 0)
+                {
+                    Console.WriteLine("Girilen sayı çifttir.");
+
+                }
+                else { Console.WriteLine("Girilen sayı tektir."); }
+
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
